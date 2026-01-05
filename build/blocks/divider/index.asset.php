c2123dadbfb019554ab4d6ee0001f3cb370e4bc3
Denis Korablev <info@codeweber.com>
2026-01-05 05:56:40 +0300

    +

--- build/blocks/divider/index.asset.php
+++ build/blocks/divider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fc3460ab5707117e8479');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4d7f1d311882ac25efb7');

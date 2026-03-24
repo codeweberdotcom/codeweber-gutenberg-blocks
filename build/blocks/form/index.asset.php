03087d0d046ad505c763bd6b31ccd594a177faf7
CodeWeber Dev <dev@codeweber.ru>
2026-03-24 15:17:30 +0100

    Revert "feat(form): add CF7 provider option to form block"
    This reverts commit 98f16965008be95a2abee9e5a72d389c9b1fe844.

--- build/blocks/form/index.asset.php
+++ build/blocks/form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '974a1421e6e1f4205aca');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4d5b39318303dd116a71');

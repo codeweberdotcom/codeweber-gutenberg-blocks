19d566c8ac46f29f98efb4b5895d23208e8ff3d7
CodeWeber Dev <dev@codeweber.ru>
2026-05-18 22:39:14 +0200

    build: compile form-field block (maxFiles default 1, min 1)

--- build/blocks/form-field/index.asset.php
+++ build/blocks/form-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b5a40aa5731eeb17164c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5c0dd9ca01a074045f77');

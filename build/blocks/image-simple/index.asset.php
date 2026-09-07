dfac2758768bac19866a3239df9921563b4df7ab
CodeWeber Dev <dev@codeweber.ru>
2026-09-07 11:15:55 +0200

    chore: rebuild after Scale slider range fix

--- build/blocks/image-simple/index.asset.php
+++ build/blocks/image-simple/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f05c33bafa1e55e8bb74');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '58375a034557d1d7fede');
